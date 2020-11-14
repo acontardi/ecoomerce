54af550f8468b06f533286946ffd2bddb75d694b
Contardi <antoniocontardi.junior@gmail.com>
2020-11-14 11:40:46 -0300

    page admin

---
+++ vendor/Acontardi/php-classes/src/PageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Acontardi;
+
+class PageAdmin extends Page{
+
+    public function __contruct($opts = array(), $tpl_dir = "/views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+
+}
+
+?>
